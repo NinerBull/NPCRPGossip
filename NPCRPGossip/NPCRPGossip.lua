dec23e2c3fd8bddf56a956abb6ccac6c70c4128c
NinerBull <72740816+NinerBull@users.noreply.github.com>
2022-11-30 23:25:13 +0100

    Temporarily remove Shift modifier to try fix taint on level up.

--- NPCRPGossip/NPCRPGossip.lua
+++ NPCRPGossip/NPCRPGossip.lua
@@ -1,7 +1,7 @@
 local npcrpframe = CreateFrame("Frame")
 npcrpframe:RegisterEvent("ADDON_LOADED")
 npcrpframe:RegisterEvent("PLAYER_LOGOUT")
-npcrpframe:RegisterEvent("MODIFIER_STATE_CHANGED")
+--npcrpframe:RegisterEvent("MODIFIER_STATE_CHANGED")
 
 
 
@@ -22,57 +22,67 @@ end
 
 
 npcrpframe:SetScript("OnEvent", function(self, event, arg1, arg2)
-    if event == "ADDON_LOADED" and arg1 == "NPCRPGossip" then
-	
-        
-        if NPCRPGossipEnable == nil then
-            -- No value, enable it
-            NPCRPGossipEnable = true
+
+	if InCombatLockdown() == false then
+		-- do nothing if in combat
+
+
+		if event == "ADDON_LOADED" and arg1 == "NPCRPGossip" then
 		
-        end
-		
-		if NPCRPGossipStoryline == nil then
-			NPCRPGossipStoryline = true
-		end
-		
-		
-		-- Let the user know if it's enabled or not
-		
-		if NPCRPGossipEnable == true then
-			print("|cffF58CBA<NPC RP Gossip>:|r Gossip Text is |cff00ff00ENABLED!|r Type |cffF58CBA/npcrpgossip|r to toggle.")
-			print("|cffF58CBA<NPC RP Gossip>:|r Hold down the |cffF58CBASHIFT|r key when interacting with an NPC to temporarily disable.")
-		else
-			print("|cffF58CBA<NPC RP Gossip>:|r Gossip Text is |cffff0000DISABLED!|r Type |cffF58CBA/npcrpgossip|r to toggle.")
-			print("|cffF58CBA<NPC RP Gossip>:|r Hold down the |cffF58CBASHIFT|r key when interacting with an NPC to temporarily enable.")
-		end
-		
-		ForceGossipShiftX =  NPCRPGossipEnable
-		
-		    
-    end
-	
-	if event == "MODIFIER_STATE_CHANGED" then
-	
-		if (arg1 == "LSHIFT") or (arg1 == "RSHIFT") then
 			
-			if arg2 == 1 then
-				-- SHIFT is pressed
-					
-				if NPCRPGossipEnable == true then
-					ForceGossipShiftX = false
-				else
-					ForceGossipShiftX = true
-				end
-					
-			else
-				-- SHIFT was let go 
-				ForceGossipShiftX =  NPCRPGossipEnable
+			if NPCRPGossipEnable == nil then
+				-- No value, enable it
+				NPCRPGossipEnable = true
+			
 			end
-					
+			
+			if NPCRPGossipStoryline == nil then
+				NPCRPGossipStoryline = true
+			end
+			
+			
+			-- Let the user know if it's enabled or not
+			
+			if NPCRPGossipEnable == true then
+				print("|cffF58CBA<NPC RP Gossip>:|r Gossip Text is |cff00ff00ENABLED!|r Type |cffF58CBA/npcrpgossip|r to toggle.")
+				--print("|cffF58CBA<NPC RP Gossip>:|r Hold down the |cffF58CBASHIFT|r key when interacting with an NPC to temporarily disable.")
+			else
+				print("|cffF58CBA<NPC RP Gossip>:|r Gossip Text is |cffff0000DISABLED!|r Type |cffF58CBA/npcrpgossip|r to toggle.")
+				--print("|cffF58CBA<NPC RP Gossip>:|r Hold down the |cffF58CBASHIFT|r key when interacting with an NPC to temporarily enable.")
+			end
+			
+			ForceGossipShiftX =  NPCRPGossipEnable
+			
+				
+		end
+		
+		--[[
+		if event == "MODIFIER_STATE_CHANGED" then
+		
+			if (arg1 == "LSHIFT") or (arg1 == "RSHIFT") then
+				
+				if arg2 == 1 then
+					-- SHIFT is pressed
+						
+					if NPCRPGossipEnable == true then
+						ForceGossipShiftX = false
+					else
+						ForceGossipShiftX = true
+					end
+						
+				else
+					-- SHIFT was let go 
+					ForceGossipShiftX =  NPCRPGossipEnable
+				end
+						
+			
+			end 
 		
 		end 
+		]]
 	
-	end 
+	
+	end
 	
 end)
 
@@ -82,11 +92,11 @@ function SlashCmdList.RPGOSSIPTOGGLE(msg)
     if NPCRPGossipEnable == true then
 		NPCRPGossipEnable = false
 		print("|cffF58CBA<NPC RP Gossip>:|r Gossip Text is |cffff0000DISABLED!|r Type |cffF58CBA/npcrpgossip|r to toggle.")
-		print("|cffF58CBA<NPC RP Gossip>:|r Hold down the |cffF58CBASHIFT|r key when interacting with an NPC to temporarily enable.")
+		--print("|cffF58CBA<NPC RP Gossip>:|r Hold down the |cffF58CBASHIFT|r key when interacting with an NPC to temporarily enable.")
 	else
 		NPCRPGossipEnable = true
 		print("|cffF58CBA<NPC RP Gossip>:|r Gossip Text is |cff00ff00ENABLED!|r Type |cffF58CBA/npcrpgossip|r to toggle.")
-		print("|cffF58CBA<NPC RP Gossip>:|r Hold down the |cffF58CBASHIFT|r key when interacting with an NPC to temporarily disable.")
+		--print("|cffF58CBA<NPC RP Gossip>:|r Hold down the |cffF58CBASHIFT|r key when interacting with an NPC to temporarily disable.")
 	end
 	
 	ForceGossipShiftX =  NPCRPGossipEnable
