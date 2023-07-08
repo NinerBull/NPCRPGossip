ff50674e054286261607c02b24cd3d6e9a336fd8
NinerBull <72740816+NinerBull@users.noreply.github.com>
2023-07-09 00:16:00 +0200

    Code Cleanup

--- NPCRPGossip/NPCRPGossip.lua
+++ NPCRPGossip/NPCRPGossip.lua
@@ -1,14 +1,16 @@
-local npcrpframe = CreateFrame("Frame")
-npcrpframe:RegisterEvent("ADDON_LOADED")
-npcrpframe:RegisterEvent("PLAYER_LOGOUT")
-npcrpframe:RegisterEvent("MODIFIER_STATE_CHANGED")
+local NPCRPGossip_Frame = CreateFrame("Frame")
+NPCRPGossip_Frame:RegisterEvent("ADDON_LOADED")
+NPCRPGossip_Frame:RegisterEvent("PLAYER_LOGOUT")
+NPCRPGossip_Frame:RegisterEvent("MODIFIER_STATE_CHANGED")
 
 
 local useNewAPI = true
 
-local NPCRPGossipTextName = BLUE_FONT_COLOR:WrapTextInColorCode("<Hidden NPC Gossip>")
-local NPCRPGossipTextSlash = BLUE_FONT_COLOR:WrapTextInColorCode("/hiddennpcgossip")
-local NPCRPGossipTextShift = BLUE_FONT_COLOR:WrapTextInColorCode("SHIFT")
+local NPCRPGossip_TextName = BLUE_FONT_COLOR:WrapTextInColorCode("<Hidden NPC Gossip>")
+local NPCRPGossip_TextSlash = BLUE_FONT_COLOR:WrapTextInColorCode("/hiddennpcgossip")
+local NPCRPGossip_TextShift = BLUE_FONT_COLOR:WrapTextInColorCode("SHIFT")
+
+
 
 if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) or (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC) then
 	useNewAPI = true
@@ -17,7 +19,6 @@ end
 if (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC) or (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)    then
 	useNewAPI = false
 end
-
 
 
 
@@ -36,30 +37,29 @@ else
 end
 
 
-npcrpframe:SetScript("OnEvent", function(self, event, arg1, arg2)
+NPCRPGossip_Frame:SetScript("OnEvent", function(self, event, arg1, arg2)
 
 
 		if event == "ADDON_LOADED" and arg1 == "NPCRPGossip" then
 		
 			
-			if NPCRPGossipEnable == nil then
+			if NPCRPGossip_AccountEnabled == nil then
 				-- No value, enable it
-				NPCRPGossipEnable = true
-			
+				NPCRPGossip_AccountEnabled = true
 			end
 						
 			
 			-- Let the user know if it's enabled or not
 			
-			if NPCRPGossipEnable == true then
-				print(NPCRPGossipTextName .. " Gossip Text is " .. GREEN_FONT_COLOR:WrapTextInColorCode("Enabled") ..". Type " .. NPCRPGossipTextSlash .. " to toggle.")
-				print(NPCRPGossipTextName .. " Hold down the " .. NPCRPGossipTextShift .. " key when interacting with an NPC to temporarily disable.")
+			if NPCRPGossip_AccountEnabled == true then
+				print(NPCRPGossip_TextName .. " Gossip Text is " .. GREEN_FONT_COLOR:WrapTextInColorCode("Enabled") ..". Type " .. NPCRPGossip_TextSlash .. " to toggle.")
+				print(NPCRPGossip_TextName .. " Hold down the " .. NPCRPGossip_TextShift .. " key when interacting with an NPC to temporarily disable.")
 			else
-				print(NPCRPGossipTextName .. " Gossip Text is " .. RED_FONT_COLOR:WrapTextInColorCode("Disabled") ..". Type " .. NPCRPGossipTextSlash .. " to toggle.")
-				print(NPCRPGossipTextName .. " Hold down the " .. NPCRPGossipTextShift .. " key when interacting with an NPC to temporarily enable.")
+				print(NPCRPGossip_TextName .. " Gossip Text is " .. RED_FONT_COLOR:WrapTextInColorCode("Disabled") ..". Type " .. NPCRPGossip_TextSlash .. " to toggle.")
+				print(NPCRPGossip_TextName .. " Hold down the " .. NPCRPGossip_TextShift .. " key when interacting with an NPC to temporarily enable.")
 			end
 			
-			ForceGossipShiftX =  NPCRPGossipEnable
+			ForceGossipShiftX =  NPCRPGossip_AccountEnabled
 			
 				
 		end
@@ -72,7 +72,7 @@ npcrpframe:SetScript("OnEvent", function(self, event, arg1, arg2)
 				if arg2 == 1 then
 					-- SHIFT is pressed
 						
-					if NPCRPGossipEnable == true then
+					if NPCRPGossip_AccountEnabled == true then
 						ForceGossipShiftX = false
 					else
 						ForceGossipShiftX = true
@@ -80,7 +80,7 @@ npcrpframe:SetScript("OnEvent", function(self, event, arg1, arg2)
 						
 				else
 					-- SHIFT was let go 
-					ForceGossipShiftX =  NPCRPGossipEnable
+					ForceGossipShiftX = NPCRPGossip_AccountEnabled
 				end
 						
 			
@@ -98,57 +98,61 @@ end)
 
 -- Slash toggle to disable/enable RP Gossip
 SLASH_RPGOSSIPTOGGLE1 = "/npcrpgossip"
-SLASH_RPGOSSIPTOGGLE1 = "/hiddennpcgossip"
+SLASH_RPGOSSIPTOGGLE2 = "/hiddennpcgossip"
 function SlashCmdList.RPGOSSIPTOGGLE(msg)
-    NPCRPGossipToggleX()
+    NPCRPGossip_ToggleX()
 end
 
 
 
 
 --Addon Compartment
-local NPCRPTooltip
+local NPCRPGossip_Tooltip
+
 
 function NPCRPGossip_CompartmentClick(addonName, buttonName)
-    NPCRPGossipToggleX()
+    NPCRPGossip_ToggleX()
 end
+
 
 function NPCRPGossip_CompartmentHover(addonName, buttonName)
-	if (not NPCRPTooltip) then
-		NPCRPTooltip = CreateFrame("GameTooltip", "NPCRPTooltip_Compartment", UIParent, "GameTooltipTemplate")
+	if (not NPCRPGossip_Tooltip) then
+		NPCRPGossip_Tooltip = CreateFrame("GameTooltip", "NPCRPGossip_Tooltip_Compartment", UIParent, "GameTooltipTemplate")
 	end
 	
-    NPCRPTooltip:SetOwner(buttonName, "ANCHOR_LEFT");
-	NPCRPTooltip:SetText("Hidden NPC Gossip Enabler")
-	NPCRPTooltip:AddLine(" ")
+    NPCRPGossip_Tooltip:SetOwner(buttonName, "ANCHOR_LEFT");
+	NPCRPGossip_Tooltip:SetText("Hidden NPC Gossip Enabler")
+	NPCRPGossip_Tooltip:AddLine(" ")
 	
-	if (NPCRPGossipEnable == true) then
-		NPCRPTooltip:AddLine("Gossip Text is currently " .. GREEN_FONT_COLOR:WrapTextInColorCode("Enabled") .. ".", WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+	if (NPCRPGossip_AccountEnabled == true) then
+		NPCRPGossip_Tooltip:AddLine("Gossip Text is currently " .. GREEN_FONT_COLOR:WrapTextInColorCode("Enabled") .. ".", WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
 	else
-		NPCRPTooltip:AddLine("Gossip Text is currently " .. RED_FONT_COLOR:WrapTextInColorCode("Disabled") .. ".", WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+		NPCRPGossip_Tooltip:AddLine("Gossip Text is currently " .. RED_FONT_COLOR:WrapTextInColorCode("Disabled") .. ".", WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
 	end
 	
-	NPCRPTooltip:AddLine(" ")
-	NPCRPTooltip:AddLine("Click to toggle.", GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b)
+	NPCRPGossip_Tooltip:AddLine(" ")
+	NPCRPGossip_Tooltip:AddLine("Click to toggle.", GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b)
 	
-	NPCRPTooltip:Show()
+	NPCRPGossip_Tooltip:Show()
 end
+
 
 function NPCRPGossip_CompartmentLeave(buttonName)
-	NPCRPTooltip:Hide()
+	NPCRPGossip_Tooltip:Hide()
 end
 
-function NPCRPGossipToggleX()
-	if NPCRPGossipEnable == true then
-		NPCRPGossipEnable = false
-		print(NPCRPGossipTextName .. " Gossip Text is " .. RED_FONT_COLOR:WrapTextInColorCode("Disabled") .. ". Type " .. NPCRPGossipTextSlash .. " to toggle.")
-		print(NPCRPGossipTextName .. " Hold down the " .. NPCRPGossipTextShift .. " key when interacting with an NPC to temporarily enable.")
+
+function NPCRPGossip_ToggleX()
+	if NPCRPGossip_AccountEnabled == true then
+		NPCRPGossip_AccountEnabled = false
+		print(NPCRPGossip_TextName .. " Gossip Text is " .. RED_FONT_COLOR:WrapTextInColorCode("Disabled") .. ". Type " .. NPCRPGossip_TextSlash .. " to toggle.")
+		print(NPCRPGossip_TextName .. " Hold down the " .. NPCRPGossip_TextShift .. " key when interacting with an NPC to temporarily enable.")
 	else
-		NPCRPGossipEnable = true
-		print(NPCRPGossipTextName .. " Gossip Text is " .. GREEN_FONT_COLOR:WrapTextInColorCode("Enabled") .. ". Type " .. NPCRPGossipTextSlash .. " to toggle.")
-		print(NPCRPGossipTextName .. " Hold down the " .. NPCRPGossipTextShift .. " key when interacting with an NPC to temporarily disable.")
+		NPCRPGossip_AccountEnabled = true
+		print(NPCRPGossip_TextName .. " Gossip Text is " .. GREEN_FONT_COLOR:WrapTextInColorCode("Enabled") .. ". Type " .. NPCRPGossip_TextSlash .. " to toggle.")
+		print(NPCRPGossip_TextName .. " Hold down the " .. NPCRPGossip_TextShift .. " key when interacting with an NPC to temporarily disable.")
 	end
 	
-	ForceGossipShiftX = NPCRPGossipEnable
+	ForceGossipShiftX = NPCRPGossip_AccountEnabled
 end
 
